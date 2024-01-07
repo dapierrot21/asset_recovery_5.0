08079c327b625725f2a8051939ff15ee3db8dabc
Dean Pierrot <dapierrot21@gmail.com>
2024-01-06 22:47:52 -0500

    Updates

---
+++ precache-manifest.725761e80e2380d9ca4381e0522bc807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bf54ec330a3bcd64b256573fb3e857",
+    "url": "/prestige-family-clone/index.html"
+  },
+  {
+    "revision": "da22a6178131f9398f17",
+    "url": "/prestige-family-clone/static/css/main.c7f70c02.chunk.css"
+  },
+  {
+    "revision": "9b11bfa1875c039c881f",
+    "url": "/prestige-family-clone/static/js/2.832cf6ee.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/prestige-family-clone/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "da22a6178131f9398f17",
+    "url": "/prestige-family-clone/static/js/main.af621a7b.chunk.js"
+  },
+  {
+    "revision": "3d74bbde34c3dc97c1e7",
+    "url": "/prestige-family-clone/static/js/runtime~main.95ce0d9a.js"
+  },
+  {
+    "revision": "73b7427f9c6a796b2f86e38550049af5",
+    "url": "/prestige-family-clone/static/media/home_1.73b7427f.png"
+  }
+]);
