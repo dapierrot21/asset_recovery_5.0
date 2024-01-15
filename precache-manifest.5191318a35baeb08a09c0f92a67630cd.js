f1d2947a72bedb76c956bad75fcc2b6bf5942122
Dean Pierrot <dapierrot21@gmail.com>
2024-01-15 09:07:36 -0500

    Updates

---
+++ precache-manifest.5191318a35baeb08a09c0f92a67630cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98b72c335d98635010d0cfa1377ed87",
+    "url": "/asset_recovery_5.0/index.html"
+  },
+  {
+    "revision": "3bc5122262051d92c6a7",
+    "url": "/asset_recovery_5.0/static/css/main.96785a50.chunk.css"
+  },
+  {
+    "revision": "76c1d927488fd243f7b0",
+    "url": "/asset_recovery_5.0/static/js/2.be044fe8.chunk.js"
+  },
+  {
+    "revision": "fb215e8faa2f8bd80fb19d8d3435c0bf",
+    "url": "/asset_recovery_5.0/static/js/2.be044fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a088c5bb5645acf0f533",
+    "url": "/asset_recovery_5.0/static/js/3.02587992.chunk.js"
+  },
+  {
+    "revision": "3bc5122262051d92c6a7",
+    "url": "/asset_recovery_5.0/static/js/main.f55dc9f9.chunk.js"
+  },
+  {
+    "revision": "2cecc90fa08d905d8ce7",
+    "url": "/asset_recovery_5.0/static/js/runtime-main.ad312c37.js"
+  },
+  {
+    "revision": "73b7427f9c6a796b2f86e38550049af5",
+    "url": "/asset_recovery_5.0/static/media/home_1.73b7427f.png"
+  }
+]);
