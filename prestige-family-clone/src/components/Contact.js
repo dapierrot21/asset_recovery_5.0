5d48e3c8caaf9756e79dcf90455562bbe859e51e
Dean Pierrot <dapierrot21@gmail.com>
2024-01-18 13:42:31 -0500

    updated file

--- prestige-family-clone/src/components/Contact.js
+++ prestige-family-clone/src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
     <section id="contact">
       <h2>Contact Us</h2>
       <p>
-        Call us for free consultation: <a href="tel:3464699290"></a>
+        Call us for free consultation: <a href="tel:3464699290">346-469-9290</a>
       </p>
       {/* Add more contact info here */}
     </section>
